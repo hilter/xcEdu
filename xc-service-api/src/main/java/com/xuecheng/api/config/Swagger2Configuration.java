2061b9cd44dadea745e106034c6ec57aec72e73f
AmazeLee <911711054@qq.com>
2019-03-10 22:44:17 +0800

    swagger

--- xc-service-api/src/main/java/com/xuecheng/api/config/Swagger2Configuration.java
+++ xc-service-api/src/main/java/com/xuecheng/api/config/Swagger2Configuration.java
@@ -10,7 +10,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//
+/**
+ * @author 麦客子
+ * @desc Swagger2配置类
+ * @email leeshuhua@163.com
+ * @create 2019/03/10 22:42
+ **/
 @Configuration
 @EnableSwagger2
 public class Swagger2Configuration {
