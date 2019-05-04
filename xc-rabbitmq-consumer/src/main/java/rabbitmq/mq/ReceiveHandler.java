45ee94274c29b6ea658ea72354a20f1730de57a3
AmazeLee <911711054@qq.com>
2019-05-04 16:40:43 +0800

    SpringBoot整合Rabbitmq

---
+++ xc-rabbitmq-consumer/src/main/java/rabbitmq/mq/ReceiveHandler.java
@@ -0,0 +1,22 @@
+package rabbitmq.mq;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import rabbitmq.config.RabbitmqConfig;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @create 2018-06-17 21:21
+ **/
+@Component
+public class ReceiveHandler {
+
+    @RabbitListener(queues = {RabbitmqConfig.QUEUE_INFORM_EMAIL})
+    public void send_email(String msg,Message message,Channel channel){
+        System.out.println("receive message is:"+msg);
+    }
+
+}
---
+++ xc-rabbitmq-producer/src/main/java/com/xuecheng/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng.rabbitmq;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 麦客子
+ * @desc
+ * @email leeshuhua@163.com
+ * @create 2019/05/04 11:24
+ **/
+@SpringBootApplication
+public class RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
+}
